37d11853ec2e0118fa092a49427ba5cfec71de19
Axel Reyes <rbaxel.f@gmail.com>
2022-05-05 16:13:51 -0500

    more console tests

--- app.js
+++ app.js
@@ -34,8 +34,9 @@ const assignFizzTrick = function(explorer){
     }
 };
 
-const explorersInNodeAndFizzTrick = explorersInNode.map((explorer) => assignFizzTrick(explorer));
 
+const explorersInNodeAndFizzTrick = explorersInNode.map((explorer) => assignFizzTrick(explorer));
+//console.log(explorersInNodeAndFizzTrick)
 // Part 6: Get a new list of explorers in node if the score number is divisible by 5, we need to set a new property called trick and set the value BUZZ, if not this value should be just the score
 //
 const assignBuzzTrick = function(explorer){
