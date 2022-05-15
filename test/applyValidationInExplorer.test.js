7eb1e8432ffbfc5b5ac5c3d9617af2c43d29bb6b
Axel Reyes <rbaxel.f@gmail.com>
2022-05-14 21:13:47 -0500

    solving test

--- test/applyValidationInExplorer.test.js
+++ test/applyValidationInExplorer.test.js
@@ -4,11 +4,20 @@ describe("Test suite for FizzBuzz validation", () =>{
 		const resultOfSomething = 16+2
 		expect(resultOfSomething).toBe(18);
 	});
-	test("1) Read json file", () =>{
+	/*test("1) ", () =>{
 		//código a probar
 		const Reader = require('./../lib/utils/Reader')
+		const explorersAmount = explorers.length
 		//propiedad a probar __ valor que se espera obtener
-		expect(explorer.filterByMission("node")).toBe()
-
+		expect(explorersAmount).toBe(14)
+	});
+	*/
+	test("2) Read json file", () =>{
+		//código a probar
+		const Reader = require('./../lib/utils/Reader')
+		const ExplorerService = require ('./../lib/services/ExplorerService')
+		const explorersAmount = explorers.length
+		//propiedad a probar __ valor que se espera obtener
+		expect(explorersAmount).toBe(14)
 	});
 })
