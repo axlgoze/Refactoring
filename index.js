87617f8743105083e0a60705ef757b81b6c2f793
Axel Reyes <rbaxel.f@gmail.com>
2022-05-14 20:50:58 -0500

    moving index

--- index.js
+++
@@ -1,9 +0,0 @@
-const Reader = require ("./lib/utils/Reader") //importando clase
-const ExplorerService = require ("./lib/services/ExplorerService")
-const explorers = Reader.readJsonFile("explorers.json")
-
-//console.log(ExplorerService.filterByMission(explorers, "node"))
-//console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "java"))
-console.log(ExplorerService.getExplorersUsernameByMission(explorers, "node"))
-//console.log(explorers)
-
