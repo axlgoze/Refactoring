bc19eabbd88fc00b79e14e6fc28879de87e43c87
Axel Reyes <rbaxel.f@gmail.com>
2022-05-28 20:54:12 -0500

    linter fix have runned

--- test/explorerController.test.js
+++ test/explorerController.test.js
@@ -1,25 +1,25 @@
-const ExplorerService = require ("./../lib/services/ExplorerService")
-const Reader = require("./../lib/utils/Reader")
+const ExplorerService = require ("./../lib/services/ExplorerService");
+const Reader = require("./../lib/utils/Reader");
 const explorers = Reader.readJsonFile("explorers.json");
 
 describe("Test suite for explorerControll", ()=>{
-	test("1) Read json file and return length of explorers", () =>{
+    test("1) Read json file and return length of explorers", () =>{
         	//código a probar
         	const explorersAmount = explorers.length;
 	        //propiedad a probar __ valor que se espera obtener
 	        expect(explorersAmount).toBe(15);
-	});
-	test("2) obtener lista de explorers filtrados por mision",()=>{
-		const explorers = [{mission: "node"},{mission: "node"}] //lo que recibo del archivo JSON
-		const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-		expect(explorersInNode.length).toBe(2)
-	});
-	test("3) regresar lista de usernames de explorers filtrados por mision",()=>{
-		const explorersUsername = ExplorerService.getExplorersUsernameByMission(explorers,"java")
-		expect(explorersUsername.length).toBe(5)
-	});
-	test("4) Regresar cantidad de explorers por mision",()=>{
-		const explorersAmountByMission = ExplorerService.getAmountOfExplorersByMission(explorers,"java")
-		expect(explorersAmountByMission).toBe(5)
-	});
-	})
+    });
+    test("2) obtener lista de explorers filtrados por mision",()=>{
+        const explorers = [{mission: "node"},{mission: "node"}]; //lo que recibo del archivo JSON
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(2);
+    });
+    test("3) regresar lista de usernames de explorers filtrados por mision",()=>{
+        const explorersUsername = ExplorerService.getExplorersUsernameByMission(explorers,"java");
+        expect(explorersUsername.length).toBe(5);
+    });
+    test("4) Regresar cantidad de explorers por mision",()=>{
+        const explorersAmountByMission = ExplorerService.getAmountOfExplorersByMission(explorers,"java");
+        expect(explorersAmountByMission).toBe(5);
+    });
+});
