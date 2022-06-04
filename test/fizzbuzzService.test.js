86d0c9a6ce85574b1abbf518d14af37b0b017954
Axel Reyes <rbaxel.f@gmail.com>
2022-06-04 15:54:55 -0500

    fizzbuzz testing done

--- test/fizzbuzzService.test.js
+++ test/fizzbuzzService.test.js
@@ -1,12 +1,12 @@
 const FizzbuzzService = require("./../lib/services/FizzbuzzService");
 describe("Test suite for new functionality", ()=>{
 	test("testing ApplyValidationInNumber", ()=>{
-		const explorer = [{score:5,trick:"Fizz"}];
-		const explorerScore= explorer.score;
-		const explorerRequest = FizzbuzzService.applyValidationInNumber(explorerScore);
-		const explorerTrick = explorerRequest.trick;
-		expect(explorerTrick).toBe("Buzz");
-//		expect(explorerRequest.trick).toBe("Fizz");
+//		const explorer = [{score:5,trick:"Fizz"}];
+//		const explorerScore= explorer.score;
+		const explorerRequest = FizzbuzzService.applyValidationInNumber(9);
+//		const explorerTrick = explorerRequest.trick;
+		expect(explorerRequest.trick).toBe("Fizz");
+//		expect(explorerRequest.score).toBe(5);
 	});
 
 })
